bcd04c5ff38205c35aa1fd84644b2441ed884421
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-04-27 16:08:18 -0400

    create: section projects

--- src/pages/home/sections/project/project.tsx
+++ src/pages/home/sections/project/project.tsx
@@ -1,20 +1,77 @@
-import { ProjectSectionContainer } from "./styled"
+import { ButtomPrimaryNormal, ButtomPrimaryWire } from "../../../../components/buttons/buttonPrimary"
+import { Card, ContentCards, ContentTitle, ProjectSectionContainer } from "./styled"
+import ImageDefault from '../../../../assets/images/perfilImage.png'
+import { FaGithub } from "react-icons/fa"
+import { FaDeploydog } from "react-icons/fa6"
 
 export const Project = () => {
+    const handleFunction =() => {
+        console.log("Clicou para carregar mais projetos.")
+    }
+
     return(
         <ProjectSectionContainer id="project">
            <div>
-                <div>
+                <ContentTitle>
                     <h2>Portfolio</h2>
                     <p>There are many variations of passages of Lorem Ipsum available,
                     but the majority have suffered alteration.</p>
-                </div>
-                <div>
-                    <div>Card's</div>
-                    <div>Card's</div>
-                    <div>Card's</div>
-                </div>
-                <button>Load More</button>
+                </ContentTitle>
+
+                <ContentCards>
+                    <Card>
+                        <img src={ImageDefault} alt="" />
+                        <div>
+                            <span>UI-UX DESIGNER</span>
+                            <h3>Product Admin Dashboard</h3>
+                            <p>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                            <div>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Github <FaGithub/>
+                                </ButtomPrimaryWire>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Deploy <FaDeploydog/>
+                                </ButtomPrimaryWire>
+                            </div>
+                        </div>
+                    </Card>
+                    <Card>
+                        <img src={ImageDefault} alt="" />
+                        <div>
+                            <span>UI-UX DESIGNER</span>
+                            <h3>Product Admin Dashboard</h3>
+                            <p>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                            <div>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Github <FaGithub/>
+                                </ButtomPrimaryWire>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Deploy <FaDeploydog/>
+                                </ButtomPrimaryWire>
+                            </div>
+                        </div>
+                    </Card>
+                    <Card>
+                        <img src={ImageDefault} alt="" />
+                        <div>
+                            <span>UI-UX DESIGNER</span>
+                            <h3>Product Admin Dashboard</h3>
+                            <p>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                            <div>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Github <FaGithub/>
+                                </ButtomPrimaryWire>
+                                <ButtomPrimaryWire onClick={handleFunction}>
+                                    Deploy <FaDeploydog/>
+                                </ButtomPrimaryWire>
+                            </div>
+                        </div>
+                    </Card>
+                </ContentCards>
+
+                <ButtomPrimaryNormal onClick={handleFunction}>
+                    Load More
+                </ButtomPrimaryNormal>
            </div>
         </ProjectSectionContainer>
     )
--- src/pages/home/sections/project/styled.ts
+++ src/pages/home/sections/project/styled.ts
@@ -3,6 +3,91 @@ import styled from "styled-components"
 
 export const ProjectSectionContainer = styled.section`
     width: 100%;
-    height: 100vh;
-    background-color: gray;
+    height: auto;
+    & > div{
+        width: 1320px;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        & > button{
+            margin-bottom: 50px;
+        }
+    }
 `
+
+export const ContentTitle = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 100px;
+    padding-bottom: 70px;
+    & > h2 {
+        font-size:48px ;
+        color: var(--secundary-normal);
+        font-weight: 700;
+        margin-bottom: 24px;
+    }
+    & > p{
+        font-size: 18px;
+        color: var(--tertiary-normal);
+        width: 590px;
+        max-width: 80%;
+        text-align: center;
+        letter-spacing: 2px;
+    }
+`
+export const ContentCards = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+    margin-bottom: 50px;
+`
+
+export const Card = styled.div`
+    width: 424px;
+    min-width: 320px;
+    height: 476px;
+    border: 1px solid black;
+    border-radius: 15px;
+    & > img{
+        width: 100%;
+        height: 248px;
+        object-fit: cover; 
+        border-top-right-radius: 12px;
+        border-top-left-radius: 12px;
+    }
+    & > div {
+        width: 100%;
+        padding: 30px;
+        & > span{
+            font-size: 12px;
+            color: var(--tertiary-lightPlus);
+            display: block;
+            margin-bottom: 4px;
+        }
+        & > h3 {
+            font-size: 18px;
+            margin-bottom: 12px;
+        }
+        & > p{
+            font-size: 14px;
+            color: var(--tertiary-normal);
+            margin-bottom: 20px;
+        }
+        & > div {
+            width: 100%;
+            display: flex;
+            gap: 5px;
+            justify-content: space-between;
+            & > button {
+                font-size: 20px;
+            }
+        }
+    }
+`
+
