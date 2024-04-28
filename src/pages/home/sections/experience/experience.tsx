bca52b183af02133857a0d4736710d0f49a4716f
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-04-28 10:08:07 -0400

    crerate: experience section

--- src/pages/home/sections/experience/experience.tsx
+++ src/pages/home/sections/experience/experience.tsx
@@ -1,9 +1,28 @@
-import { ExperienceSectionContainer } from "./styled"
+import { ButtomPrimaryNormal } from "../../../../components/buttons/buttonPrimary"
+import { handleClickTest } from "../../../../components/utils"
+import { ExperienceCard, ExperienceSectionContainer, ExpirienceCardsContent, ExpirienceDetailsContent } from "./styled"
 
 export const Experience = () => {
     return(
         <ExperienceSectionContainer id="experience">
-            <h1>Experiencia profissional</h1>
+            <div>
+                <ExpirienceDetailsContent>
+                    <h2>What I do?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla purus arcu, varius eget velit non, laoreet imperdiet orci. Mauris ultrices eget lorem ac vestibulum. Suspendis imperdiet,</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla purus arcu, varius eget velit non.</p>
+                    <ButtomPrimaryNormal onClick={handleClickTest}>
+                        Say Hellow!
+                    </ButtomPrimaryNormal>
+                </ExpirienceDetailsContent>
+
+                <ExpirienceCardsContent>
+                    <div>
+                        <ExperienceCard>card 01</ExperienceCard>
+                        <ExperienceCard>card 02</ExperienceCard>
+                        <ExperienceCard>card 03</ExperienceCard>
+                    </div>
+                </ExpirienceCardsContent>
+            </div>
         </ExperienceSectionContainer>
     )
 }
--- src/pages/home/sections/experience/styled.ts
+++ src/pages/home/sections/experience/styled.ts
@@ -3,6 +3,66 @@ import styled from "styled-components"
 export const ExperienceSectionContainer = styled.section`
     width: 100%;
     height: 100vh;
-    background-color: red;
+    background-color: var(--tertiary-light);
+    & > div{
+        width: 1320px;
+        max-width: 100%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
 
+`
+
+export const ExpirienceDetailsContent = styled.div`
+    width: 50%;
+    min-width: 529px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    @media(max-width: 768px){
+        text-align: center;
+    }
+    & > h2{
+        font-size: 48px;
+        color: var(--secundary-normal);
+        font-weight: 700;
+        margin-bottom: 24px;
+    }
+    & > p {
+        font-size: 18px;
+        color: var(--tertiary-normal);
+        margin-bottom: 16px;
+        width: 80%;
+    }
+    & > button{
+        margin-top: 32px;
+    }
+
+`
+export const ExpirienceCardsContent = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    & > div {
+        width: 648px;
+        height: 576px;
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+    
+`
+
+export const ExperienceCard = styled.div`
+    width: 100%;
+    height: 176px;
+    background-color: white;
+    border-left: 5px solid var(--primary-normal);
+    border-radius: 6px;
 `
